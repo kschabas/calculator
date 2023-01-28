451e9fc47b63c0963b7dc76385e1dc858cb52263
Karl Schabas <karl.schabas@utoronto.ca>
2023-01-27 20:47:25 -0500

    Wire up click events

--- calculator.js
+++ calculator.js
@@ -1,16 +1,18 @@
 function operate(a,b,operator) {
+    aint = parseInt(a);
+    bint = parseInt(b);
     switch (operator) {
         case '+':
-            return a + b;
+            return aint + bint;
 
         case '-':
-            return a-b;
+            return aint-bint;
         
         case '*':
-            return a * b;
+            return aint * bint;
 
         case '/':
-            return a / b;
+            return aint / bint;
 
         default:
             return NaN;
@@ -24,10 +26,101 @@ function displayMessage(msg) {
 }
 
 
-var a;
+var a = "";
 var aPresent = false;
-var b;
+var b = "";
+var operator = "";
 var bPresent = false;
 
-displayMessage("Testing!!!");
+initializeCalc();
+
+function processKey(key) {
+    if (key >='0' && key <='9') {
+        if (!aPresent) {
+            a += key;
+            displayMessage(a);
+        }
+        else {
+            b += key;
+            displayMessage(b);
+        }
+    }
+    if (key == '+' || key == '-' || key == '*' || key == '/') {
+        processOperator(key);
+    }
+    if (key == '=') {
+        processEquals();
+    }
+    if (key == 'Clear') {
+        clear();
+    }
+}
+
+function processOperator(key) {
+    if (!aPresent) {
+        aPresent = true;
+        operator = key;
+    }
+    else {
+        var num = operate(a,b,operator);
+        displayMessage(num);
+        a = num;
+        b = "";
+        operator = key;
+    }
+}
+
+function processEquals() {
+    if (!a || !b || !operator) displayMessage("ERROR!");
+    var num = operate(a,b,operator);
+    displayMessage(num);
+    a = "";
+    b = "";
+    aPresent = false;
+}
+
+function initializeCalc() {
+    var button = document.querySelector('#one');
+    button.addEventListener('click',(e) => processKey('1'));
+
+    button = document.querySelector('#two');
+    button.addEventListener('click',(e) => processKey('2'));
+
+    button = document.querySelector('#three');
+    button.addEventListener('click',(e) => processKey('3'));
+
+    button = document.querySelector('#four');
+    button.addEventListener('click',(e) => processKey('4'));
+
+    button = document.querySelector('#five');
+    button.addEventListener('click',(e) => processKey('5'));
+
+    button = document.querySelector('#six');
+    button.addEventListener('click',(e) => processKey('6'));
+
+    button = document.querySelector('#seven');
+    button.addEventListener('click',(e) => processKey('7'));
+
+    button = document.querySelector('#eight');
+    button.addEventListener('click',(e) => processKey('8'));
+
+    button = document.querySelector('#nine');
+    button.addEventListener('click',(e) => processKey('9'));
+
+    button = document.querySelector('#add');
+    button.addEventListener('click',(e) => processKey('+'));
+
+    button = document.querySelector('#subtract');
+    button.addEventListener('click',(e) => processKey('-'));
+
+    button = document.querySelector('#multiply');
+    button.addEventListener('click',(e) => processKey('*'));
+
+    var button = document.querySelector('#divide');
+    button.addEventListener('click',(e) => processKey('/'));
+
+    var button = document.querySelector('#equal');
+    button.addEventListener('click',(e) => processKey('='));
+
+}
 
